2c93b82a822d5b7bafbc6a1916cdd0af790e99bb
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-04-05 11:20:54 -0700

    Uncomment indexhtml

--- indexHtml.js
+++ indexHtml.js
@@ -5,19 +5,19 @@
  * application code into before sending it to the client as regular HTML.
  * Note we're returning a template string from this function.
  */
-// const indexHtml = (highlights, highlights_bundle) => `
-//   <!DOCTYPE html>
-//   <html>
-//     <head>
-//       <title>Yelp Highlights</title>
-//     </head>
-//     <body>
-//       ${highlights}
-//       <script>${highlights_bundle}</script>
-//     </body>
-//   </html>
-// `;
+const indexHtml = (highlights, highlights_bundle) => `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Yelp Highlights</title>
+    </head>
+    <body>
+      ${highlights}
+      <script>${highlights_bundle}</script>
+    </body>
+  </html>
+`;
 
 // export default indexHtml;
 
-// module.exports = indexHtml;
+module.exports = indexHtml;
